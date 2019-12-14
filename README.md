96ce68b7a2c04cc51b3ccff84c08a11190c5f184
stewie15 <dtrinkl015@gmail.com>
2019-12-14 11:20:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SSY-Transaktionen_New
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.send('NodeJS + Express läuft!');
-});
-
-module.exports = router;
